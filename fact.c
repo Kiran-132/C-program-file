75fc04797fa068185c9d789a874505332335067c
kiran <kiranhegadi132@gmail.com>
2025-09-27 09:25:13 +0000

    Added C program files

---
+++ fact.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void factorial()
+
+{
+	int n, i;
+	unsigned long long fact = 1;
+	printf("\nEnter an integer: ");
+	scanf("%d", &n);
+
+
+	if (n < 0)
+		printf("Error! Factorial of a negative number doesn't exist.");
+	else
+	{
+		for (i = 1; i <= n; ++i)
+		{
+			fact *= i;
+		}
+		printf("Factorial of %d is = %llu\n");
+	}
+
+	//return 0;
+
+}
